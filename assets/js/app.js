4ea46617f482da01ab3609d8df04a913ec991fe8
Colin Shaw <colin.shaw400@outlook.com>
2022-03-11 23:10:49 +0000

    Add div for scores in HTML and declaring the random property for selecting the computers selction with math.random

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,22 @@
 // declare variables 
 const playerChoice = document.getElementById("player-choice");
 const computerChoice = document.getElementById("computer-choice");
+const computerScoreElement = document.getElementById("computer-score");
+const playerScoreElement = document.getElementById("player-score");
 const result = document.getElementById("results-choice");
 
-let userSelection
-let compChoice
-let results
-/***
- * adding function for event listener when any button is clicked a selection is passed through and event
- * Mehtod for declaring function learnt on W3 schools 
- */
+let results;
+let playerScore = 0;
+let computerScore = 0;
 
-
-/***
- * Declareing function for generating the computer choice and the difficulty
- */
-function generateComputerChoice(){
-    const randomNumber = Math.floor(Math.random() * choices.length);
-    
-    if(randomNumber === 1){
-        compChoice = 'rock'
-    }
-    if(randomNumber === 2){
-        compChoice = 'paper'
-    }
-    if(randomNumber === 3){
-        compChoice = 'scissors'
-    }
-    if(randomNumber === 4){
-        compChoice = 'lizard'
-    }
-    if(randomNumber === 5){
-        compChoice = 'spock'
-    }
-    computerChoice.innerHTML = compChoice.toUpperCase();
-}
-
-//hold all choices in a variable so the get result functions can generate a selection easier
- const CHOICES = {
+const CHOICES = {
     rock: 'rock',
     paper: 'paper',
     scissors: 'scissors',
     spock: 'spock',
     lizard: 'lizard'
 }
-/***
- * declare function for checking result from game choices
- */
+
 const getResults = (playerChoice, computerChoice) => {
     if (playerChoice === computerChoice) {
        return result.innerHTML = "This round is a tie!";
@@ -110,9 +80,31 @@ const getRandomProperty = () => {
     return CHOICES[keys[keys.length * Math.random() << 0]]
 }
 
+const updateScore = (result) => {
+    console.log('result.contains: ', result.includes('Computer'));
+    
+     if(result.includes('Computer')) {
+        computerScore++
+     }
+     if(result.includes('Player')) {
+         playerScore++
+     }
+    computerScoreElement.innerHTML = computerScore.toString();
+     playerScoreElement.innerHTML = playerScore.toString();
+}
+
+/*
+ * Declareing function for generating the computer choice and the difficulty
+ */
+const generateComputerChoice = () => {
+    const randomProperty = getRandomProperty();
+    return computerChoice.innerHTML = randomProperty
+}
+
 const onButtonClick = (id) => {
     const aiChoice = generateComputerChoice();
     playerChoice.innerHTML = id.toUpperCase();
     computerChoice.innerHTML = aiChoice.toUpperCase();
-    getResults(id.toLowerCase(), aiChoice.toLowerCase());
+    const result = getResults(id.toLowerCase(), aiChoice.toLowerCase());
+    updateScore(result);
 }
